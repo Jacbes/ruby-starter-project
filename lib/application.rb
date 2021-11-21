7b061f57f00b0534d004e690bd1129756a5bdba0
Jacbes <skishon100000@gmail.com>
2021-11-21 22:40:58 +0700

    Change states

--- lib/application.rb
+++ lib/application.rb
@@ -1,13 +1,12 @@
-require_relative 'context'
-require_relative 'app_states/input'
+require_relative 'app_states/input_from'
 require_relative 'app_states/exit'
 
 class Application
   def run
-    context = Context.new(Input.new)
-    until context.state.is_a? Exit
-      context.render
-      context.next
+    state = InputFrom.new
+    until state.is_a? Exit
+      state.render
+      state = state.next
     end
   end
 end
